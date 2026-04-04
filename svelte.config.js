585ab04bcbd5c3a08166a56baabd0f985d987ae9
Soorman <sirtv490@gmail.com>
2026-04-04 15:22:33 +0600

    Layout.js and Package Installations

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 import { relative, sep } from 'node:path';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -13,7 +13,11 @@ const config = {
 			return isExternalLibrary ? undefined : true;
 		}
 	},
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter(),
+			paths: {
+			base: process.env.NODE_ENV === "production" ? "/cool-portofolio" : "",
+		},
+	 }
 };
 
 export default config;
