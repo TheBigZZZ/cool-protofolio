57a0e764bc3639bed74744b8ce8419641eb05977
Soorman <sirtv490@gmail.com>
2026-04-06 20:45:39 +0600

    Removed /

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 	},
 	kit: { adapter: adapter(),
 			paths: {
-			base: process.env.NODE_ENV === "production" ? "/" : "",
+			base: process.env.NODE_ENV === "production" ? "" : "",
 		},
 	 }
 };
